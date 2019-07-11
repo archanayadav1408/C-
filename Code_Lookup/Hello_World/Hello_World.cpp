467200c5ec3483270606040f26c35c6ce5bc4695
Devsingh640 <devsingh640@gmail.com>
2019-07-11 22:46:33 +0530

    This is basics i am learning

--- Code_Lookup/Hello_World/Hello_World.cpp
+++ Code_Lookup/Hello_World/Hello_World.cpp
@@ -2,8 +2,12 @@
 
 int main()
     {
-        std::cout << "Hello World\n";
-        return 0;
-        
-        
+        std::cout << "Hello World.";                             // this will print string on screen.
+        std::cout << "Look i got appended at the same line.";    // this will append on same line.
+        std::cout << std:: endl;                                 // this will append a complete line end and move to next line.
+        std::cout << "We moved to second line due to endl.";     // this will print string on screen.
+        std::cout << "\n";                                       // this will append a complete line end and move to next line due to \n.
+        std::cout << "We moved to second line due to 'back slash'n";     // this will append a complete line end and move to next line.
+
+        return 0;                                                // this will return 0 at the end of this main function.
     }
---
+++ Puzzle_Games/Story_Puzzle/Story_Puzzle.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<iostream>
+
+int main()
+    {
+        std::cout << "PROJECT KINGS-EMPIRE";
+        std::cout << std:: endl;
+        std::cout << "Enter your username : ";
+        std::cout << std:: endl;
+        std::cout << "Enter your password : ";
+        std::cout << std:: endl;
+
+            return (0);
+    }
+
+
